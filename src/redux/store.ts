a59e3c20696f71098985499aad4160a635a83746
nirjhorsaha <nirjhor.badhan25@gmail.com>
2024-07-03 11:45:21 +0600

    [Implement] logger

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -6,7 +6,7 @@ export const store = configureStore({
     reducer: {
         counter: counterSlice
     },
-    // middleware: (getDefaultMiddleware)=> getDefaultMiddleware().concat(Logger)
+    middleware: (getDefaultMiddleware)=> getDefaultMiddleware().concat(Logger)
 })
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
